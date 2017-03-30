069499e28d21b5a72c9c89f8a2069cbf2943cb2c
liubo <liubo404@gmail.com>
2017-03-30 09:48:32 +0800

    sdf

---
+++ src/main/java/jcf/ArrayListTest.java
@@ -0,0 +1,13 @@
+package jcf;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Thinkpad on 2017/3/29.
+ */
+public class ArrayListTest {
+    public static void main(String[] args){
+        ArrayList<Integer> alist = new ArrayList<>();
+        alist.add(2);
+    }
+}
